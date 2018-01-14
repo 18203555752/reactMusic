ef7713566331d1a6858134208ab837db8551806b
wjp <519688153@qq.com>
2018-01-14 23:39:20 +0800

    Signed-off-by: wjp <519688153@qq.com>

--- app/components/progress.js
+++ app/components/progress.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './progress.less';
 
 let Progress = React.createClass({
+  changeTime(e){
+
+      var progressBar=this.refs.progressBar;
+      var progressBar_ing=(e.clientX-progressBar.getBoundingClientRect().left)/progressBar.clientWidth;
+      
+      this.props.progress_handle(progressBar_ing)
+  },
   render(){
       return (
-        <div className="pregress">
-          <div className="clear_float">
-              <img src="/static/images/logo.png" width="40px" />
-              <span>{this.props.aa}</span>
+        <div className="pregress" ref="progressBar" onClick={this.changeTime}>
+          <div className="clear_float pregressIng" style={{width:`${this.props.progress.abs}%`}}
+            >
+              <div className="pregressIng_son">{this.props.progress.str}</div>
           </div>
         </div>
       );
--- app/root.js
+++ app/root.js
@@ -15,14 +15,29 @@ let Root = React.createClass({
                     mp3:"/static/mp3/mp3.mp3"
                 }).jPlayer('play');
             }
+        });
+        $("#player").bind($.jPlayer.event.timeupdate,(e) =>{
+            this.setState({
+                progress:{
+                  str:Math.round(e.jPlayer.status.currentTime)+"秒",
+                  abs:e.jPlayer.status.currentPercentAbsolute,
+                }
+            });
+            this.allTime=e.jPlayer.status.duration;
         })
     },
-
+    allTime:'',
+    componentWillUnmount(){
+      $("#player").unbind($.jPlayer.event.timeupdate)
+    },
+    progress_handle:function(son){
+      $("#player").jPlayer('play',this.allTime*son)
+    },
     render(){
         return(
             <div>
               <Header />
-              <Progress aa="默默001"/>
+              <Progress progress_handle={this.progress_handle} progress={this.state.progress} aa="默默001"/>
             </div>
         )
     }
