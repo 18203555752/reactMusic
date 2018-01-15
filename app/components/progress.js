8465453488e0fe4670d53f01ae99ba31442aa290
unknown <jinpengw@corp.longshine.com>
2018-01-15 16:49:02 +0800

    Signed-off-by: unknown <jinpengw@corp.longshine.com>

--- app/components/progress.js
+++ app/components/progress.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './progress.less';
 
 let Progress = React.createClass({
+    getDefaultProps(){
+        return {
+            bacColor:"#2f9842",
+        }
+    },
   changeTime(e){
-
       var progressBar=this.refs.progressBar;
       var progressBar_ing=(e.clientX-progressBar.getBoundingClientRect().left)/progressBar.clientWidth;
-      
       this.props.progress_handle(progressBar_ing)
   },
   render(){
       return (
         <div className="pregress" ref="progressBar" onClick={this.changeTime}>
-          <div className="clear_float pregressIng" style={{width:`${this.props.progress.abs}%`}}
+          <div className="clear_float pregressIng" style={{width:`${this.props.progress.abs}%`;background:`${this.props.bacColor}%`}}
             >
               <div className="pregressIng_son">{this.props.progress.str}</div>
           </div>
---
+++ app/page/player.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './progress.less';
+
+let Progress = React.createClass({
+
+ 
+})
+
+export default Progress;
